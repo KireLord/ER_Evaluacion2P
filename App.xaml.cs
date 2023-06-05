ff71f20db56d78e8ad38a4d4580ea37854e44da9
Erick Rosero <Erick Rosero@LAPTOP-ERICK>
2023-06-05 07:32:42 -0500

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ER_Evaluacion2P;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ER_Evaluacion2P;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
